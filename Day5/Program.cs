2c0e5acc4f78ceeb6d9ba24d560dc3a3cc8f453a
Max <max@maximilian.co.nz>
2022-12-07 01:31:20 +1300

    Completely restarting

--- Day5/Program.cs
+++ Day5/Program.cs
@@ -1,51 +1,45 @@
-﻿class Program
+﻿using System.Text.RegularExpressions;
+
+class Program
 {
-    
-    public static void Main(string[] args)
-    {
+	
+	public static void Main(string[] args)
+	{
 
-        // Get the data
-        string[] data = File.ReadAllLines("./data.txt");
+		// Get the data
+		string[] data = File.ReadAllLines("./data.txt");
 
-        // Parse the first part of the data
-        List<string> crateStack1 = new List<string>();
-        List<string> crateStack2 = new List<string>();
-        List<string> crateStack3 = new List<string>();
-        List<string> crateStack4 = new List<string>();
-        List<string> crateStack5 = new List<string>();
-        List<string> crateStack6 = new List<string>();
-        List<string> crateStack7 = new List<string>();
-        List<string> crateStack8 = new List<string>();
-        List<string> crateStack9 = new List<string>();
-
-        // Loop through all the crates and add them to a list
-        List<char>[] crates = new List<char>[8];
-        for (int i = 0; i < 8; i++)
-        {
-            string currentCrateRow = data[0..8][i];
-            crates[i] = currentCrateRow.ToList<char>();
-        }
-
-        // Remove all the ascii art
-        foreach (List<char> crate in crates)
-        {
-            foreach (char crateItem in crate)
-            {
-                crate.Remove(' ');
-                crate.Remove('[');
-                crate.Remove(']');
-            }
-        }
-
-        for (int i = 0; i < crates.Count(); i++)
-        {
-            for (int j = 0; j < crates[i].Count(); j++)
-            {
-                Console.WriteLine(crates[i][j]);
-            }
-        }
+	}
 
 
-        // Parse the second part of the data
-    }
+
+
+
+
+
+
+
+	// Print the contents of an array
+	static void DebugArray<T>(T[] array, string? title = null)
+	{
+		if (title != null) Console.Write(title + " array: ");
+		for (int i = 0; i < array.Length; i++)
+		{
+			Console.Write(array[i]); 
+			if (i != array.Length - 1) Console.Write(", ");
+		}
+		Console.WriteLine();
+	}
+
+	// Print the contents of a list
+	static void DebugList<T>(List<T> list, string? title = null)
+	{
+		if (title != null) Console.Write(title + " list: ");
+		for (int i = 0; i < list.Count; i++)
+		{
+			Console.Write(list[i]);
+			if (i != list.Count - 1) Console.Write(", ");
+		}
+		Console.WriteLine();
+	}
 }
