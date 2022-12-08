e310bca29078e111aea5dcfa45a0ceb83a23e363
Max <max@maximilian.co.nz>
2022-12-09 03:13:40 +1300

    Finished day 6, part 1

--- Day6/part1.py
+++ Day6/part1.py
@@ -1,11 +1,11 @@
 # Get the data
 data = open("./Day6/data.txt", "r").read()
 
-size_of_packet_marker = 0
+start_of_packet_marker = 0
 
 # Loop through each character in the data
 for i in range(len(data)):
-    size_of_packet_marker += 1
+    start_of_packet_marker += 1
 
     # Get groups of 4 at each index
     packet = [data[i], data[i + 1], data[i + 2], data[i + 3]]
@@ -14,5 +14,5 @@ for i in range(len(data)):
     if len(packet) == len(set(packet)):
 
         # Show the answer
-        print("The first packet begins at the index", size_of_packet_marker + 3)
+        print("The first packet begins at the index", start_of_packet_marker + 3)
         break
