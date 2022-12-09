e8406a24be28914cb7c318e4dfe9e40dd5ce33bd
Max <max@maximilian.co.nz>
2022-12-09 14:37:48 +1300

    Added directory and file classes

---
+++ Day7/Directory.java
@@ -0,0 +1,10 @@
+package Day7;
+
+import java.util.List;
+
+public class Directory {
+    
+    public String name;
+    public List<Day7.File> contents;
+
+}
---
+++ Day7/File.java
@@ -0,0 +1,9 @@
+package Day7;
+
+public class File {
+    
+    public String name;
+    public int size;
+    public Directory location;
+
+}
