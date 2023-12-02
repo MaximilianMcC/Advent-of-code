ccb81f5347d5ad445690470833f91ad73d2ad11f
Max <max@maximilian.co.nz>
2023-12-02 20:55:33 +1300

    Day 2 part 1

---
+++ 2023/Day2/Program.cs
@@ -0,0 +1,11 @@
+﻿class Program
+{
+	public static void Main(string[] args)
+	{
+		// string[] data = File.ReadAllLines("./test.txt");
+		string[] data = File.ReadAllLines("./data.txt");
+
+		Console.WriteLine(Part1.Solution(data));
+
+	}
+}
