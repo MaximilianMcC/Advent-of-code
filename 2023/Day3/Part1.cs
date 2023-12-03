65b5704ad13d58a8d453e8bc4f448a3015afbe65
Max <max@maximilian.co.nz>
2023-12-04 01:08:08 +1300

    idk bruh

--- 2023/Day3/Part1.cs
+++ 2023/Day3/Part1.cs
@@ -8,6 +8,9 @@ class Part1
 		// all of the symbols neighbors. If one of the neighbors is a number, then keep
 		// searching on the X of that current line until the entire number has been discovered.		
 
+		// Replace the `.`'s with empty strings to make stuff easier
+		schematic = schematic.Select(str => str.Replace('.', ' ')).ToArray();
+
 		// Loop through all characters in the schematic
 		for (int y = 0; y < schematic.Length; y++)
 		{
@@ -16,15 +19,86 @@ class Part1
 				// Get the character
 				char character = schematic[y][x];
 
-				// Check for if the character isn't a number, or a `.` (symbol)
-				if (character == '.' && int.TryParse(character.ToString(), out _)) continue;
+				// Check for if the character is a symbol (not an int)
+				if (int.TryParse(character.ToString(), out _)) continue;
 
 				// Get all of the neighbors, and check for if its a number
-				List<Tuple<int, int>> numberLocations = new List<Tuple<int, int>>();
-				if (int.TryParse(schematic[y][x - 1].ToString(), out _)) numberLocations.Add(new Tuple<int, int>(y, x - 1));
+				List<Tuple<int, int>> neighbors = GetNeighbors(x, y, schematic);
+				foreach (Tuple<int, int> numberPosition in neighbors)
+				{
+					// Keep on going along the x values for each number until the end is reached
+					int startX = numberPosition.Item1;
+					int xIndex = 0;
+					string fullNumber = schematic[numberPosition.Item2][numberPosition.Item1].ToString();
+					
+					//? When reading the schematic it looks like the longest
+					//? number that can be possible is three characters, so will
+					//? search two characters out from the confirmed starting index
+					const int lookAhead = 3;
+
+					for (int i = 0; i < lookAhead; i++)
+					{
+						// Check for if we have already found all the numbers and exit
+						if (fullNumber.Length >= 3) break;
+
+						int number = 0;
+						xIndex++;
+
+						// Forwards/right
+						int forwardX = startX + xIndex;
+						if (forwardX < schematic.Length && int.TryParse(schematic[forwardX][numberPosition.Item2].ToString(), out number))
+						{
+							// Number was found going right
+							xIndex++;
+							fullNumber = fullNumber + number.ToString();
+						}
+
+						// Backwards/left
+						int backwardX = startX - xIndex;
+						if (backwardX >= 0 && int.TryParse(schematic[backwardX][numberPosition.Item2].ToString(), out number))
+						{
+							// Number was found going left
+							xIndex--;
+							fullNumber = number.ToString() + fullNumber;
+						}
+					}
+				
+					// Update the sum with new number
+					sum += int.Parse(fullNumber);
+				}
 			}
 		}
 
 		return sum;
 	}
+
+	// TODO: Do in a cleaner way that doesn't make it look like a .min.js file!!
+	// TODO: Replace tuple for vector2
+	private static List<Tuple<int, int>> GetNeighbors(int x, int y, string[] grid)
+	{
+		List<Tuple<int, int>> neighbors = new List<Tuple<int, int>>();
+
+		// Check top row
+		if (y - 1 >= 0)
+		{
+			if (x - 1 >= 0 && int.TryParse(grid[y - 1][x - 1].ToString(), out _)) neighbors.Add(new Tuple<int, int>(y - 1, x - 1));
+			if (int.TryParse(grid[y - 1][x].ToString(), out _)) neighbors.Add(new Tuple<int, int>(y - 1, x));
+			if (x + 1 < grid[0].Length && int.TryParse(grid[y - 1][x + 1].ToString(), out _)) neighbors.Add(new Tuple<int, int>(y - 1, x + 1));
+		}
+
+		// Check middle row
+		if (x - 1 >= 0 && int.TryParse(grid[y][x - 1].ToString(), out _)) neighbors.Add(new Tuple<int, int>(y, x - 1));
+		if (x + 1 < grid[0].Length && int.TryParse(grid[y][x + 1].ToString(), out _)) neighbors.Add(new Tuple<int, int>(y, x + 1));
+
+		// Check bottom row
+		if (y + 1 < grid.Length)
+		{
+			if (x - 1 >= 0 && int.TryParse(grid[y + 1][x - 1].ToString(), out _)) neighbors.Add(new Tuple<int, int>(y + 1, x - 1));
+			if (int.TryParse(grid[y + 1][x].ToString(), out _)) neighbors.Add(new Tuple<int, int>(y + 1, x));
+			if (x + 1 < grid[0].Length && int.TryParse(grid[y + 1][x + 1].ToString(), out _)) neighbors.Add(new Tuple<int, int>(y + 1, x + 1));
+		}
+
+		return neighbors;
+	}
+
 }
