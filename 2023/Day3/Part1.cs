64cdee078a2eb184172a0b35eadd8dcc14c7c52c
Max <max@maximilian.co.nz>
2023-12-04 23:53:02 +1300

    Restarted day 3

--- 2023/Day3/Part1.cs
+++ 2023/Day3/Part1.cs
@@ -1,3 +1,5 @@
+using System.Numerics;
+
 class Part1
 {
 	public static int Solution(string[] schematic)
@@ -8,63 +10,25 @@ class Part1
 		// all of the symbols neighbors. If one of the neighbors is a number, then keep
 		// searching on the X of that current line until the entire number has been discovered.		
 
-		// Replace the `.`'s with empty strings to make stuff easier
+		// Replace the `.`'s with empty strings to make parsing numbers easier
 		schematic = schematic.Select(str => str.Replace('.', ' ')).ToArray();
 
-		// Loop through all characters in the schematic
-		for (int y = 0; y < schematic.Length; y++)
+		// Loop through all characters in the schematic, but ignore the edges
+		for (int i = 1; i < schematic.Length - 1; i++)
 		{
-			for (int x = 0; x < schematic[y].Length; x++)
+			for (int j = 1; j < schematic[i].Length - 1; j++)
 			{
-				// Get the character
-				char character = schematic[y][x];
+				// Check for if we are on a symbol (not a number or space)
+				if (int.TryParse(schematic[i][j].ToString(), out _) || schematic[i][j] == ' ') continue;
 
-				// Check for if the character is a symbol (not an int)
-				if (int.TryParse(character.ToString(), out _)) continue;
-
-				// Get all of the neighbors, and check for if its a number
-				List<Tuple<int, int>> neighbors = GetNeighbors(x, y, schematic);
-				foreach (Tuple<int, int> numberPosition in neighbors)
-				{
-					// Keep on going along the x values for each number until the end is reached
-					int startX = numberPosition.Item1;
-					int xIndex = 0;
-					string fullNumber = schematic[numberPosition.Item2][numberPosition.Item1].ToString();
-					
-					//? When reading the schematic it looks like the longest
-					//? number that can be possible is three characters, so will
-					//? search two characters out from the confirmed starting index
-					const int lookAhead = 3;
-
-					for (int i = 0; i < lookAhead; i++)
-					{
-						// Check for if we have already found all the numbers and exit
-						if (fullNumber.Length >= 3) break;
-
-						int number = 0;
-						xIndex++;
-
-						// Forwards/right
-						int forwardX = startX + xIndex;
-						if (forwardX < schematic.Length && int.TryParse(schematic[forwardX][numberPosition.Item2].ToString(), out number))
-						{
-							// Number was found going right
-							xIndex++;
-							fullNumber = fullNumber + number.ToString();
-						}
-
-						// Backwards/left
-						int backwardX = startX - xIndex;
-						if (backwardX >= 0 && int.TryParse(schematic[backwardX][numberPosition.Item2].ToString(), out number))
-						{
-							// Number was found going left
-							xIndex--;
-							fullNumber = number.ToString() + fullNumber;
-						}
-					}
+				// Get the coordinates of any adjacent/neighboring numbers
+				List<Vector2> adjacentNumbers = GetNeighbors(schematic, new Vector2(i, j));
 				
-					// Update the sum with new number
-					sum += int.Parse(fullNumber);
+				// Loop through all of the numbers on the same x axis until
+				// the entire number is revealed both forwards and back
+				foreach (Vector2 numberPosition in adjacentNumbers)
+				{
+					int currentNumber = schematic[(int)numberPosition.X][(int)numberPosition.Y];
 				}
 			}
 		}
@@ -72,33 +36,37 @@ class Part1
 		return sum;
 	}
 
-	// TODO: Do in a cleaner way that doesn't make it look like a .min.js file!!
-	// TODO: Replace tuple for vector2
-	private static List<Tuple<int, int>> GetNeighbors(int x, int y, string[] grid)
+
+
+	private static List<Vector2> GetNeighbors(string[] schematic, Vector2 startPosition)
 	{
-		List<Tuple<int, int>> neighbors = new List<Tuple<int, int>>();
+		List<Vector2> neighbors = new List<Vector2>();
 
-		// Check top row
-		if (y - 1 >= 0)
+		// Use a position map to check for neighbors in nicer written way then hardcoding
+		// each value/coordinate by hand
+		// TODO: Just map x values, then increase/decrease y 
+		Vector2[][] positionMap = {
+			new Vector2[3] { new Vector2(-1, 1), new Vector2(0, 1), new Vector2(1, 1) },
+			new Vector2[3] { new Vector2(-1, 0), new Vector2(0, 0), new Vector2(1, 0) },
+			new Vector2[3] { new Vector2(-1, -1), new Vector2(0, -1), new Vector2(1, -1) }
+		};
+
+		for (int i = 0; i < 3; i++)
 		{
-			if (x - 1 >= 0 && int.TryParse(grid[y - 1][x - 1].ToString(), out _)) neighbors.Add(new Tuple<int, int>(y - 1, x - 1));
-			if (int.TryParse(grid[y - 1][x].ToString(), out _)) neighbors.Add(new Tuple<int, int>(y - 1, x));
-			if (x + 1 < grid[0].Length && int.TryParse(grid[y - 1][x + 1].ToString(), out _)) neighbors.Add(new Tuple<int, int>(y - 1, x + 1));
-		}
+			for (int j = 0; j < 3; j++)
+			{
+				// Get the position of the current thingy relative to the
+				// starting position that was given
+				int x = (int)startPosition.X + (int)positionMap[i][j].X;
+				int y = (int)startPosition.Y + (int)positionMap[i][j].Y;
+				Vector2 position = new Vector2(x, y);
 
-		// Check middle row
-		if (x - 1 >= 0 && int.TryParse(grid[y][x - 1].ToString(), out _)) neighbors.Add(new Tuple<int, int>(y, x - 1));
-		if (x + 1 < grid[0].Length && int.TryParse(grid[y][x + 1].ToString(), out _)) neighbors.Add(new Tuple<int, int>(y, x + 1));
-
-		// Check bottom row
-		if (y + 1 < grid.Length)
-		{
-			if (x - 1 >= 0 && int.TryParse(grid[y + 1][x - 1].ToString(), out _)) neighbors.Add(new Tuple<int, int>(y + 1, x - 1));
-			if (int.TryParse(grid[y + 1][x].ToString(), out _)) neighbors.Add(new Tuple<int, int>(y + 1, x));
-			if (x + 1 < grid[0].Length && int.TryParse(grid[y + 1][x + 1].ToString(), out _)) neighbors.Add(new Tuple<int, int>(y + 1, x + 1));
+				// Check for if the thing at the position is a number
+				if (int.TryParse(schematic[(int)position.X][(int)position.Y].ToString(), out _) == false) continue;
+				neighbors.Add(position);
+			}
 		}
 
 		return neighbors;
 	}
-
 }
